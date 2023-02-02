bfdd0b85cb4965783a3784b92e50f0e1f2aa8b41
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-02 11:29:42 +0100

    jsVariablenHolen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
 console.log("test");
+
+// Variablen
+
+let ausblenden = document.querySelector(".ausblenden");
+// ausblenden.style.display = "none";
+let einblenden = document.querySelector(".einblenden");
+
+let radio_rundenZahl_05 = document.querySelector("#radio_rundenZahl_05");
+let radio_rundenZahl_10 = document.querySelector("#radio_rundenZahl_10");
+let radio_rundenZahl_15 = document.querySelector("#radio_rundenZahl_15");
+let radio_rundenZahl_20 = document.querySelector("#radio_rundenZahl_20");
+
+let rundenZahl_Text = document.querySelector("#rundenZahl_Text");
+// 0 : 0 
+
+let wuerfelErgebnisUser = document.querySelector("#wuerfelErgebnisUser");
+let wuerfelErgebnisComp = document.querySelector("#wuerfelErgebnisComp");
+
+let wuerfelErgebnis_Text = document.querySelector("#wuerfelErgebnis_Text");
+
+let button_0 = document.querySelector("#button_0");
+let button_1 = document.querySelector("#button_1");
+let button_2 = document.querySelector("#button_2");
+
+let button_reset = document.querySelector("#button_reset");
+// reset = Seite neu laden    oder   alle Variablen auf 0
